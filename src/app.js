a26cec513116e6fe81e13a42eb1a21199884998d
Oleg Shkalikov <maska6293@yandex.ru>
2022-08-23 17:29:24 +0300

    fix: allow cors

--- src/app.js
+++ src/app.js
@@ -53,6 +53,28 @@ app.use(
   )
 );
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+
+  // authorized headers for preflight requests
+  // https://developer.mozilla.org/en-US/docs/Glossary/preflight_request
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+
+  app.options('*', () => {
+    // allowed XHR methods
+    res.header(
+      'Access-Control-Allow-Methods',
+      'GET, PATCH, PUT, POST, DELETE, OPTIONS'
+    );
+    res.send();
+  });
+
+  next();
+});
+
 app.use('/words', wordRouter);
 
 app.use('/signin', signinRouter);
